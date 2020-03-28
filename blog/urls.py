3605cb9c11705bede5180796874f95d2c80115ba
wolfy00 <rk432000@gmail.com>
2020-03-28 06:38:02 +0530

    added css

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-] + staticfiles_urlpatterns()
+]
